88e9fde66f8e9de71b1ae08b4214254691ac484e
krasvl <vlkrasulenkov@yandex.ru>
2025-01-08 22:40:35 +0300

    Revert "iter9"
    This reverts commit 22a8fcb06de7a9c8da2a08d9dbd6ed56e62603b2.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,15 +7,7 @@ import (
 
 func main() {
 	addrDefault := "localhost:8080"
-	intervalDefault := 300
-	fileDefault := "./store"
-	restore := true
-
-	srv, err := server.GetConfiguredServer(addrDefault, intervalDefault, fileDefault, restore)
-
-	if err != nil {
-		log.Fatalf("Server configure error: %v", err)
-	}
+	srv := server.GetConfiguredServer(addrDefault)
 
 	if err := srv.Start(); err != nil {
 		log.Fatalf("Server error: %v", err)
--- internal/server/server_configure.go
+++ internal/server/server_configure.go
@@ -2,46 +2,23 @@ package server
 
 import (
 	"flag"
-	"fmt"
 	"os"
-	"strconv"
 
 	"go.uber.org/zap"
 
 	"metrics/internal/storage"
 )
 
-func GetConfiguredServer(
-	addrDefault string,
-	intervalDefault int,
-	fileDefault string,
-	restoreDefault bool) (*Server, error) {
+func GetConfiguredServer(addrDefault string) *Server {
 	addr := flag.String("a", addrDefault, "address")
-	interval := flag.Int("i", intervalDefault, "interval")
-	file := flag.String("f", fileDefault, "file")
-	restore := flag.Bool("r", restoreDefault, "restore")
 
 	flag.Parse()
 
 	if value, ok := os.LookupEnv("ADDRESS"); ok && value != "" {
 		addr = &value
 	}
-	if value, ok := os.LookupEnv("STORE_INTERVAL"); ok && value != "" {
-		parsed, err := strconv.ParseInt(value, 10, 32)
-		if err == nil && parsed >= 0 {
-			valueint := int(parsed)
-			interval = &valueint
-		}
-	}
-	if value, ok := os.LookupEnv("FILE_STORAGE_PATH"); ok && value != "" {
-		file = &value
-	}
-	if value, ok := os.LookupEnv("RESTORE"); ok && value != "" {
-		parsed, err := strconv.ParseBool(value)
-		if err == nil {
-			restore = &parsed
-		}
-	}
+
+	memStorage := storage.NewMemStorage()
 
 	logger, _ := zap.NewProduction()
 	defer func() {
@@ -50,10 +27,5 @@ func GetConfiguredServer(
 		}
 	}()
 
-	fileStorage, err := storage.NewFileStorage(*file, *interval, *restore)
-	if err != nil {
-		return nil, fmt.Errorf("cant create fileStorage: %w", err)
-	}
-
-	return NewServer(*addr, fileStorage, logger), nil
+	return NewServer(*addr, memStorage, logger)
 }
